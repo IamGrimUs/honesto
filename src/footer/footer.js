0756a2aa99db3d59afdba6a84bc6db70b351a960
Russell Thomas <iamgrimus@gmail.com>
2020-11-07 16:06:02 -0800

    app navigation components & react router

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import './footer.styles.css';
 
-export const Footer = ({text}) => {
+export const Footer = () => {
   return (
     <footer>
       <section className="container flex-row flex-row_between center">
-      <div>
-        <figure>
-          <img src="/Theorem.png" alt="Theorem Logo" />
-        </figure>
-      </div>
-      <div>
-        <p className="text-align-right">{text}</p>
+        <div>
+          <figure>
+            <img src="/Theorem.png" alt="Theorem Logo" />
+          </figure>
+        </div>
+        <div>
+          <p className="text-align-right">
+            Copyright © 2018 <span className="text-bold">Theorem</span>, LLC. All Rrights Reserved.
+          </p>
         </div>
       </section>
     </footer>
-  )
-}
+  );
+};
---
+++ src/siteNav/siteNav.js
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { CurrentUser } from "../currentUser/currentUser";
+import { Bug } from "../bug/bug";
+
+import "./siteNav.styles.css";
+
+export const SiteNav = () => {
+  const User = {
+    name: "Kim Trantow",
+    image: "https://s3.amazonaws.com/uifaces/faces/twitter/rdsaunders/128.jpg",
+  };
+  const feedbackNeeded = {
+    outstanding: 2,
+  };
+  const feedbackRecieved = {
+    recieved: 8,
+  };
+
+  return (
+    <div className="site-navigation">
+      <div className="container center flex-row flex-row_between">
+        <div className="logo-container">
+          <Link to="/">
+            <span className="logo-text">Honesto</span>
+          </Link>
+        </div>
+        <ul className="site-links-container">
+          <li>
+            {feedbackNeeded ? <Bug text={feedbackNeeded.outstanding} /> : null}
+            <Link to="">Share Feedback</Link>
+          </li>
+          <li>
+            {feedbackRecieved ? <Bug text={feedbackRecieved.recieved} /> : null}
+            <Link to="">My Feedback</Link>
+          </li>
+          <li>
+            <Link to="">Team Feedback</Link>
+          </li>
+          <li>
+            <Link to="">Teams</Link>
+          </li>
+        </ul>
+        <div className="sub-grid flex-row flex-row_between">
+          <div className="feedback-cycle-container text-align-right">
+            <p>Next Feedback Cycle:</p>
+            <p>
+              <span className="month">December 2020</span> - <span className="days-until-review">22 days</span>
+            </p>
+          </div>
+          <div className="divider">&nbsp;</div>
+          <CurrentUser user={User} logout="true" />
+        </div>
+      </div>
+    </div>
+  );
+};
